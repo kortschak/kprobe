52822a6627ef26eb2d1e71d6f8a82913a4ba49e0
Dan Kortschak <dan@kortschak.io>
2021-10-24 22:30:59 +1030

    fix documentation for example returned type

--- kprobe_unpacker_example_test.go
+++ kprobe_unpacker_example_test.go
@@ -58,7 +58,7 @@ func (u Unpacker) Register(format io.Reader) (name string, err error) {
 }
 
 // Unpack parses the provided date and returns the name of the event and
-// a struct holding the event details.
+// a pointer to a struct holding the event details.
 func (u Unpacker) Unpack(data []byte) (string, reflect.Value, error) {
 	if len(data) < 8 {
 		return "", reflect.Value{}, io.ErrUnexpectedEOF
